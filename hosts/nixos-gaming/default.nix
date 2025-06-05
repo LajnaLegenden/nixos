429c22bf711f087ebc72d028afe386934fc9bba4
LajnaLegenden <34426335+LajnaLegenden@users.noreply.github.com>
2025-06-05 10:05:39 +0200

    fix

--- hosts/nixos-gaming/default.nix
+++ hosts/nixos-gaming/default.nix
@@ -35,8 +35,18 @@
 #:w
 
 # networking.hostName = "nixos"; # Define your hostname.
-
 {
+  users.users.remotebuild = {
+    isNormalUser = true;
+    createHome = false;
+    group = "remotebuild";
+
+    openssh.authorizedKeys.keyFiles = [ ./remotebuild.pub ];
+  };
+
+  users.groups.remotebuild = {};
+
+  nix.settings.trusted-users = [ "remotebuild" ];
   imports = [
     ../common
     ./configuration.nix
