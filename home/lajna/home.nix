8d7080f70431c89fdf4b664bcb6b333cade422fa
Linus Jansson <linus.jansson@mediatool.com>
2025-06-16 15:31:06 +0200

    asd

--- home/lajna/home.nix
+++ home/lajna/home.nix
@@ -66,6 +66,13 @@
       window = {
         opacity = 0.8;
       };
+      font = {
+      normal = { family = "FiraCode Nerd Font"; style = "Regular"; };
+      bold = { family = "FiraCode Nerd Font"; style = "Bold"; };
+      italic = { family = "FiraCode Nerd Font"; style = "Italic"; };
+      bold_italic = { family = "FiraCode Nerd Font"; style = "Bold Italic"; };
+      size = 12;
+    };
     };
   };
 
--- hosts/pong/hardware-configuration.nix
+++ hosts/pong/hardware-configuration.nix
@@ -51,7 +51,9 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
   # networking.interfaces.wwan0.useDHCP = lib.mkDefault true;
-powerManagement.enable = true;
+  powerManagement.enable = true;
+  services.power-profiles-daemon.enable = true;
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
